26efef2c96dd06b9fea81af8e804efdea0e40551
John Hume <duelin.markers@gmail.com>
2013-08-21 17:35:19 -0500

    Rename file->ns-sym to src->ns-sym

--- src/duelinmarkers/insfactor/nrepl.clj
+++ src/duelinmarkers/insfactor/nrepl.clj
@@ -3,14 +3,14 @@
             [clojure.tools.nrepl.middleware :as middleware]
             [duelinmarkers.insfactor :as insfactor]))
 
-(defn file->ns-sym [src]
+(defn src->ns-sym [src]
   (symbol (fnext (re-find #"\(ns\s+([a-zA-Z0-9.*+!?_-]+)" src))))
 
 (defn index-on-load [h]
   (fn [{:keys [op file] :as msg}]
     (h msg)
     (when (= op "load-file")
-      (insfactor/index! (file->ns-sym file)))))
+      (insfactor/index! (src->ns-sym file)))))
 
 (middleware/set-descriptor! #'index-on-load
                             {:expects #{"load-file"}})
--- test/duelinmarkers/insfactor/nrepl_test.clj
+++ test/duelinmarkers/insfactor/nrepl_test.clj
@@ -1,7 +1,7 @@
 (ns duelinmarkers.insfactor.nrepl-test
   (:use clojure.test duelinmarkers.insfactor.nrepl))
 
-(deftest of-file->ns-sym
-  (is (= 'foo.bar (file->ns-sym "(ns foo.bar)")))
-  (is (= 'foo.bar (file->ns-sym "(ns foo.bar\n  (:require foo.baz))")))
-  (is (= 'foo.bar (file->ns-sym "(set! *warn-on-reflection* true)\n(ns foo.bar\n  \"some docs\")"))))
+(deftest of-src->ns-sym
+  (is (= 'foo.bar (src->ns-sym "(ns foo.bar)")))
+  (is (= 'foo.bar (src->ns-sym "(ns foo.bar\n  (:require foo.baz))")))
+  (is (= 'foo.bar (src->ns-sym "(set! *warn-on-reflection* true)\n(ns foo.bar\n  \"some docs\")"))))
