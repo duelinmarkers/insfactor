aba48ef412b5bcad2f98a3afc5d777b5deb4604b
John Hume <duelin.markers@gmail.com>
2014-11-16 16:51:47 -0600

    Upgrade nrepl to 0.2.6.

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.analyzer.jvm "0.6.4"]
-                 [org.clojure/tools.nrepl "0.2.3"]]
+                 [org.clojure/tools.nrepl "0.2.6"]]
   :repl-options {:nrepl-middleware [duelinmarkers.insfactor.nrepl/index-on-load]}
   )
